5ff783cc50a430820d6d1fe868d21d3e3fe11e1d
HyeongJunJeon <wehj0105@gmail.com>
2022-08-15 22:26:42 +0900

    [SOLVE] 50

---
+++ problem 100(js)/problem 41~50/50. 버블정렬 구현하기.js
@@ -0,0 +1,25 @@
+function bubble(arr) {
+  let result = arr.slice(); // 원본 배열 복사
+
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = 0; j < result.length - i; j++) {
+      if (result[j] > result[j + 1]) {
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+  return result;
+}
+
+const items = prompt("입력해주세요.")
+  .split(" ")
+  .map((n) => {
+    return parseInt(n, 10);
+  });
+
+console.log(bubble(items));
+
+//거품정렬은 인접한 두 요소의 크기를 비교해서 정렬한다고 하는데
+//temp변수를 왜 선언해서 임시값을 넣어주는지 이해하지 못했다..
